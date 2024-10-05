ce45cc337a3fb138a6f8427bdf7dbe69518a203d
Rkislam3390990 <ri3390990@gmail.com>
2024-10-05 14:12:14 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config= {
+    theme: {
+      extend: {
+        colors: {
+        //   clifford: '#da373d',
+        },
+        //  Background's start
+        backgroundImage: {
+            headerBack:"url('images/HeaderBackground.png')",
+            berries:"url('images/Background.png')",
+            portfolio:"url('images/Moutain.png')",
+          },
+        //  Background's End
+        // Container's Start
+        spacing: {
+            'headerContainer': '1320px',
+            'aboutME': '1066px',
+            'skills': '1004px',
+          },
+       // Container's end
+       // Font family Start
+       'display': ['Poppins'],
+       // Font family end
+      }
+    }
+  }
